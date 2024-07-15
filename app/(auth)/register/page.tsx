1354c2fc2c38b26fc57f738547a13a2fd8a7fd88
nerwinalamas <nerwinalamas@gmail.com>
2024-07-15 10:33:24 +0800

    feat: initial commit

---
+++ app/(auth)/register/page.tsx
@@ -0,0 +1,80 @@
+"use client";
+
+import Link from "next/link";
+import { signup } from "./actions";
+import { useState } from "react";
+
+const Register = () => {
+    const [isPasswordShowing, setIsPasswordShowing] = useState(false);
+
+    return (
+        <div className="min-h-screen flex items-center justify-center bg-slate-800 text-slate-100">
+            <form className="w-80 p-5 rounded-lg flex flex-col gap-5 bg-slate-900">
+                <h1 className="text-center text-xl font-semibold">TaskGuard</h1>
+                <div className="flex flex-col gap-1">
+                    <label htmlFor="email" className="text-sm cursor-pointer">
+                        Email:
+                    </label>
+                    <input
+                        type="email"
+                        name="email"
+                        id="email"
+                        required
+                        placeholder="Email"
+                        className="p-2 text-slate-900"
+                    />
+                </div>
+                <div className="flex flex-col gap-1">
+                    <label
+                        htmlFor="password"
+                        className="text-sm cursor-pointer"
+                    >
+                        Password:
+                    </label>
+                    <input
+                        type={isPasswordShowing ? "text" : "password"}
+                        name="password"
+                        id="password"
+                        required
+                        placeholder="Password"
+                        className="p-2 text-slate-900"
+                    />
+                    <div className="flex gap-2">
+                        <input
+                            type="checkbox"
+                            id="showPassword"
+                            className="cursor-pointer"
+                            checked={isPasswordShowing}
+                            onChange={(e) =>
+                                setIsPasswordShowing(e.target.checked)
+                            }
+                        />
+                        <label
+                            htmlFor="showPassword"
+                            className="text-sm cursor-pointer"
+                        >
+                            Show password
+                        </label>
+                    </div>
+                </div>
+                <button
+                    formAction={signup}
+                    className="p-2 font-semibold bg-white text-black"
+                >
+                    Register
+                </button>
+                <div className="w-full flex justify-center gap-2 text-sm">
+                    <p>Already have an account?</p>
+                    <Link
+                        href="/login"
+                        className="hover:underline text-blue-500"
+                    >
+                        <p>Login</p>
+                    </Link>
+                </div>
+            </form>
+        </div>
+    );
+};
+
+export default Register;
