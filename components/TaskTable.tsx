082acf9d4006847f067ca55bf95a26c82cf1417a
nerwinalamas <nerwinalamas@gmail.com>
2024-07-21 11:07:31 +0800

    refactor: task table

--- components/TaskTable.tsx
+++ components/TaskTable.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import {
     Table,
     TableBody,
@@ -8,30 +6,16 @@ import {
     TableHeader,
     TableRow,
 } from "@/components/ui/table";
-import useModal from "@/hooks/useModal";
-import { Input } from "./ui/input";
-import { Button } from "./ui/button";
-import { useState } from "react";
+import CreateTask from "./CreateTask";
+import TaskAction from "./TaskAction";
+import { tasks } from "@/app/task/action";
 
-const TaskTable = () => {
-    const { openModal } = useModal();
-    const [task, setTask] = useState("");
+const TaskTable = async () => {
+    const { data } = await tasks();
 
     return (
-        <div className="w-1/2 flex flex-col gap-1">
-            <form className="w-full flex items-center justify-end gap-2">
-                <Input
-                    type="text"
-                    name="task"
-                    id="task"
-                    placeholder="Create Task"
-                    required
-                    className="w-60"
-                    value={task}
-                    onChange={(e) => setTask(e.target.value)}
-                />
-                <Button variant="secondary">Create</Button>
-            </form>
+        <div className="xl:w-1/2 flex flex-col gap-1">
+            <CreateTask />
             <Table className="text-slate-100">
                 <TableHeader>
                     <TableRow>
@@ -42,31 +26,21 @@ const TaskTable = () => {
                     </TableRow>
                 </TableHeader>
                 <TableBody>
-                    {Array(10)
-                        .fill(null)
-                        .map((data) => (
-                            <TableRow>
-                                <TableCell>1</TableCell>
-                                <TableCell>Create Table</TableCell>
-                                <TableCell>Pending</TableCell>
-                                <TableCell className="flex gap-5">
-                                    <button
-                                        onClick={() =>
-                                            openModal("updateTaskModal")
-                                        }
-                                    >
-                                        Edit
-                                    </button>
-                                    <button
-                                        onClick={() =>
-                                            openModal("deleteTaskModal")
-                                        }
-                                    >
-                                        Delete
-                                    </button>
-                                </TableCell>
-                            </TableRow>
-                        ))}
+                    {data?.map((task) => (
+                        <TableRow key={task.id}>
+                            <TableCell>{task.id}</TableCell>
+                            <TableCell className="capitalize">{task.task}</TableCell>
+                            <TableCell className="capitalize">{task.status}</TableCell>
+                            <TableCell className="flex gap-5">
+                                <TaskAction modalName="updateTaskModal" data={task}>
+                                    Edit
+                                </TaskAction>
+                                <TaskAction modalName="deleteTaskModal" data={task}>
+                                    Delete
+                                </TaskAction>
+                            </TableCell>
+                        </TableRow>
+                    ))}
                 </TableBody>
             </Table>
         </div>
