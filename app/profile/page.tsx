f0b6cf9c084bbfd3f0357d927f17e4a969071266
nerwinalamas <nerwinalamas@gmail.com>
2024-07-22 09:07:41 +0800

    styles: profile component

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,8 +1,19 @@
+import { createClient } from "@/utils/supabase/server";
 
-const Profile = () => {
-  return (
-    <div>Profile</div>
-  )
-}
+const Profile = async () => {
+    const supabase = await createClient();
+    const { data, error } = await supabase.auth.getUser();
 
-export default Profile
+    if (error) {
+        return <p>{error.message}</p>;
+    }
+
+    return (
+        <div className="p-4 lg:px-10 lg:py-5">
+            <h1 className="text-xl font-bold">Profile</h1>
+            <p>Email: {data.user.email}</p>
+        </div>
+    );
+};
+
+export default Profile;
