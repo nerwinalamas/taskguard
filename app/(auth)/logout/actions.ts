d498276c41b14b4d29c082da1ddfbf94bcef30a3
nerwinalamas <nerwinalamas@gmail.com>
2024-07-22 09:06:57 +0800

    refactor: logout actions

---
+++ app/(auth)/logout/actions.ts
@@ -0,0 +1,18 @@
+"use server";
+
+import { createClient } from "@/utils/supabase/server";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+
+export async function logout() {
+    const supabase = createClient();
+    const { error } = await supabase.auth.signOut();
+
+    if (error) {
+        console.log("error message: ", error.message);
+        console.log("error status: ", error.status);
+    }
+
+    revalidatePath("/");
+    redirect("/login");
+}
