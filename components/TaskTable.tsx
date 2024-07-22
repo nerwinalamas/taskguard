d962f6d71ec3243ce7996524fc410b1ccee955bd
nerwinalamas <nerwinalamas@gmail.com>
2024-07-22 09:14:50 +0800

    refactor: tasktable component

--- components/TaskTable.tsx
+++ components/TaskTable.tsx
@@ -1,48 +1,32 @@
-import {
-    Table,
-    TableBody,
-    TableCell,
-    TableHead,
-    TableHeader,
-    TableRow,
-} from "@/components/ui/table";
+import { Table, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 import CreateTask from "./CreateTask";
-import TaskAction from "./TaskAction";
 import { tasks } from "@/app/task/action";
+import TaskTableBody from "./TaskTableBody";
+import TaskTablePagination from "./TaskTablePagination";
 
 const TaskTable = async () => {
     const { data } = await tasks();
 
     return (
-        <div className="xl:w-1/2 flex flex-col gap-1">
+        <div className="xl:w-1/2 flex flex-col gap-5">
             <CreateTask />
             <Table className="text-slate-100">
                 <TableHeader>
-                    <TableRow>
+                    <TableRow className="hidden lg:w-full lg:grid lg:grid-cols-4">
                         <TableHead>id</TableHead>
                         <TableHead>Task</TableHead>
                         <TableHead>Status</TableHead>
                         <TableHead>Action</TableHead>
                     </TableRow>
                 </TableHeader>
-                <TableBody>
-                    {data?.map((task) => (
-                        <TableRow key={task.id}>
-                            <TableCell>{task.id}</TableCell>
-                            <TableCell className="capitalize">{task.task}</TableCell>
-                            <TableCell className="capitalize">{task.status}</TableCell>
-                            <TableCell className="flex gap-5">
-                                <TaskAction modalName="updateTaskModal" data={task}>
-                                    Edit
-                                </TaskAction>
-                                <TaskAction modalName="deleteTaskModal" data={task}>
-                                    Delete
-                                </TaskAction>
-                            </TableCell>
-                        </TableRow>
-                    ))}
-                </TableBody>
+                <TaskTableBody data={data || []} count={data?.length || 0} />
             </Table>
+            {data && data.length > 10 && (
+                <TaskTablePagination
+                    data={data || []}
+                    count={data?.length || 0}
+                />
+            )}
         </div>
     );
 };
